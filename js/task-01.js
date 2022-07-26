0f39da5d6198aaa6117238064508943fb3bd8e19
Oleksii Bykovskyi <bikovskij@ukr.net>
2022-07-26 22:30:08 +0300

    add revision

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,7 @@
 const totalCategories = document.querySelectorAll(".item");
 console.log(`Number of categories: ${totalCategories.length}`);
-// const categoriesEl = [...totalCategories]
-totalCategories.forEach(
-  (categories) => `Category: ${categories.children[0].textContent} 
-  Elements: ${categories.children[1].children.length}`
-);
-// .join("\n");
-console.log(totalCategories);
+
+totalCategories.forEach((categories) => {
+  console.log(`Category: ${categories.children[0].textContent}
+    Elements: ${categories.children[1].children.length}`);
+});
